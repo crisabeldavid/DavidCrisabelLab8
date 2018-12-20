6edc102dc43da217fc0e7abed3fc95fd2b90f1df
crisabeldavid <crisabel.david.iics@ust.edu.ph>
2018-12-20 20:34:22 +0800

    Initial commit

---
+++ app/src/main/java/com/example/user_pc/davidcrisabellab8/Information.java
@@ -0,0 +1,16 @@
+package com.example.user_pc.davidcrisabellab8;
+
+public class Information {
+
+    public String fullname, gender;
+    public int age;
+    public Information () {
+    }
+
+    public Information (String fullname, int age, String gender){
+        this.fullname = fullname;
+        this.age = age;
+        this.gender = gender;
+    }
+
+}
